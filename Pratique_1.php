108a7cec80040cdf2d97f740fc991412dca0b5d3
Emilia2496 <69214513+Emilia2496@users.noreply.github.com>
2020-08-09 22:30:12 +0100

    Pratique.php

---
+++ Pratique_1.php
@@ -0,0 +1,19 @@
+<?php
+/*Affichage d'une chaîne de caractère*/
+echo  'Orange Pink Green <br>';
+/*Affichage d'une variable contenant un entier*/
+$x = 5;
+echo "$x <br>";
+/*Affichage d'une chaîne de caractère faisant apparaître une variable*/
+$age = 16;
+echo "I have $age years old <br>";
+/*Affichage d'un texte différent selon qu'une condition est vraie ou fausse*/
+$y = 8;
+$z = 5;
+if ($y > $z) {
+echo"Five is less than eight <br>";
+}
+else {
+echo"Impossible <br>";
+}
+?>
---
+++ Pratique_2.php
@@ -0,0 +1,19 @@
+
+<?php
+/*Affichage des entiers de 1 à 10 à l'aide d'une boucle while*/
+$n = 1;
+while ($n<=10) {
+    echo "$n <br>";
+    $n++;
+}
+/*Affichage des entiers de 1 à 10 à l'aide d'une boucle for*/
+for ($n = 1; $n <= 10; $n++) {
+    echo"$n <br>";
+}
+/*Affichage de 1 à un nombre quelconque*/
+$n = 1;
+while ($n >= 1) {
+    echo "$n <br>";
+    $n++;
+}
+?>
